dbf30f6cafe4a2b6e29371a1b4400a3432911a5a
Chylli <chylli@binary.com>
2018-03-26 17:12:11 +0800

    refactor arch

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,4 @@
 extern crate regex;
-use regex::Regex;
 
-struct Path {
-    path : String,
-}
-
-fn path(path_name: &str) -> Path {
-    Path{
-        path: path_name.to_string()
-    }
-}
-
-fn canon_path(path_name: &str) -> String {
-    let re1 = Regex::new(r"^/\.\.|//").unwrap();
-    let mut result =  path_name.to_string();
-    while re1.is_match(&result){
-        result = re1.replace_all(&result, "/").to_string();
-    }
-    result
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn path_works() {
-        let path_name = "tmp/a.txt";
-        let p1 = super::path(path_name);
-        assert_eq!(p1.path, path_name);
-        assert_eq!(super::canon_path("/../../hello"), "/hello");
-        assert_eq!(super::canon_path("///hello"), "/hello");
-        assert_eq!(super::canon_path("//../hello"), "/hello");
-    }
-}
+pub mod path;
+pub use path::Path;
