b3ab85055043977dcfa2e419f5536849b30b76bd
Chylli <chylli@binary.com>
2018-03-26 14:23:19 +0800

    cannao path

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,26 @@
-#[allow(dead_code)]
+extern crate regex;
+use regex::Regex;
+
 struct Path {
     path : String,
 }
-#[allow(dead_code)]
+
 fn path(path_name: &str) -> Path {
     Path{
         path: path_name.to_string()
     }
+}
+
+fn canon_path(path_name: &str) -> String {
+    // remove /../..
+    let re1 = Regex::new(r"//").unwrap();
+    let re2 = Regex::new(r"^/\.\.").unwrap();
+    let mut result =  path_name.to_string();
+    while re1.is_match(&result) || re2.is_match(&result){
+        let string1 = re1.replace_all(&result, "/").to_string();
+        result = re2.replace_all(&string1,"/").to_string();
+    }
+    result
 }
 
 #[cfg(test)]
@@ -16,5 +30,8 @@ mod tests {
         let path_name = "tmp/a.txt";
         let p1 = super::path(path_name);
         assert_eq!(p1.path, path_name);
+        assert_eq!(super::canon_path("/../../hello"), "/hello");
+        assert_eq!(super::canon_path("///hello"), "/hello");
+        assert_eq!(super::canon_path("//../hello"), "/hello");
     }
 }
